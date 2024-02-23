897a64aa84c786ba106e81b6c34987db122bcb59
NAGARJUNREDDY\nagar <nagarjun.sdet@gmail.com>
2024-02-23 13:28:15 +0530

    cucumber

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,14 @@
 package pages;
 
+import org.openqa.selenium.By;
+
 public class LoginPage {
+	
+	public final By username=By.name("username");
+	public final By password=By.name("password");
+	public final By btnLogin=By.xpath("//button[@type='submit']");
+	public final By errorUserName=By.xpath("(//span[contains(@class,'oxd-input-field-error-message')])[1]");
+	public final By errorPassword=By.xpath("(//span[contains(@class,'oxd-input-field-error-message')])[2]");
+	public final By errorUserPassword=By.xpath("//div[contains(@class,'oxd-alert-content--error')]/p");
 
 }
